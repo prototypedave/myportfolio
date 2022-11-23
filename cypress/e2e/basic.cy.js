4ef628a4881134c403af13ad6c8fea9f4306db7f
prototypedave <popupgfj@gmail.com>
2022-11-23 15:53:42 -0800

    added a class element to the test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,7 @@ describe('sample test', () => {
   })
 
   it('displays the resources text', () => {
-    cy.get('SectionText#cty')
-    .contains('Solving tech problems is my passion, have a look at some of my projects below');
+    cy.get('home')
+    .contains('David Isumba');
   })
 })
