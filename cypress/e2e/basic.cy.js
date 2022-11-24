08af9acf1bd1c15cd904ed33faab26a60e81b002
prototypedave <popupgfj@gmail.com>
2022-11-23 16:06:37 -0800

    test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,8 +3,11 @@ describe('sample test', () => {
     cy.visit('/')
   })
 
-  it('displays the resources text', () => {
-    cy.get('home')
-    .contains('David Isumba');
+  it('renders the Netlify logo image', () => {
+    cy.get('img')
+    .should('be.visible')
+    .and(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0);
+    })
   })
 })
