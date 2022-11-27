64a65002f6ead2fd7d53ca7b6b1dadbc4a127ca8
prototypedave <popupgfj@gmail.com>
2022-11-26 19:05:15 -0800

    updated mobile view for the navbar

---
+++ components/html-parts/Mobile.js
@@ -0,0 +1,8 @@
+export default function Mobile() {
+  var x = document.getElementById("myNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
